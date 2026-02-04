dde2165a03a92558dcf060798592f099def832dc
Aditya <adityadombe709@gmail.com>
2026-02-05 01:06:35 +0530

    created login and signup pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,30 @@
 import { Dashboard } from "./pages/Dashboard";
+import { BrowserRouter, Routes, Route, Link, Navigate } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import { useState } from "react";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
-    <Dashboard />
-  )
+    <BrowserRouter>
+      {/* <Dashboard /> */}
+      <Routes>
+        <Route
+          path="/"
+          element={<Login setIsLoggedIn={setIsLoggedIn} />}
+        ></Route>
+        <Route
+          path="/signup"
+          element={<Signup setIsLoggedIn={setIsLoggedIn} />}
+        ></Route>
+        <Route
+          path="/dashboard"
+          element={isLoggedIn ? <Dashboard /> : <Navigate to="/" />}
+        ></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
